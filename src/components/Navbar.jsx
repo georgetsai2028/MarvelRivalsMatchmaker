4ab1dd2fd0c4a5b0cbe057f9b7af3e607fc6c1dd
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-12 19:27:23 -0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar(){
+    return(
+        <nav>
+            <h1>Marvel Rivals Matchmaker</h1>
+            <ul>
+                <li>
+                <a href='/'>Home</a>
+                </li>
+                <li> <a href='/teams'>Teams</a>
+                </li>
+                <li>
+                    <a href='/profile'>Profile</a>
+                </li>
+
+            </ul>
+        </nav>
+    );
+}

566888c8ae1cd7da2049f4fe876947df12beacab
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-13 02:27:11 -0700

    moved mock player data in profile add css to clean up

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,36 @@
+const mockUserData = {
+    username: "Puka",
+    avatarUrl: "https://some-avatar-url.com",
+    rank: "Gold",
+    teamsJoined: ["Avengers", "X-Men", "Fantastic Four"],
+    achievements: ["MVP of the Month", "Top Scorer"],
+    playerStats: {
+      gamesPlayed: 150,
+      wins: 100,
+      losses: 50,
+    },
+  };
+
 export default function Profile() {
-    return <div>Your profile page!</div>;
-}  
+    return (
+
+
+        <div>
+          <h1>Profile</h1>
+          <img src={mockUserData.avatarUrl} alt="User Avatar" />
+          <p>Username: {mockUserData.username}</p>
+          <p>Rank: {mockUserData.rank}</p>
+          <p>Teams Joined: {mockUserData.teamsJoined.join(', ')}</p>
+          <ul>
+            <h3>Achievements:</h3>
+            {mockUserData.achievements.map((achievement, index) => (
+              <li key={index}>{achievement}</li>
+            ))}
+          </ul>
+          <p>Games Played: {mockUserData.playerStats.gamesPlayed}</p>
+          <p>Wins: {mockUserData.playerStats.wins}</p>
+          <p>Losses: {mockUserData.playerStats.losses}</p>
+        </div>
+      );
+    }
+    

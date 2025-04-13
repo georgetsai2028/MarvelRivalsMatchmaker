aa6cdd23ac43b20e013f23b1c14a12cd20bf3a47
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-04-13 02:11:12 -0700

    changed dropdown option values

--- src/components/TeamFIlters.jsx
+++ src/components/TeamFIlters.jsx
@@ -91,9 +91,12 @@ export default function TeamFilters({ onFilter }) {
             className="select"
           >
             <option value="">Max Players</option>
-            <option value="6">6</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
             <option value="4">4</option>
-            <option value="8">8</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
           </select>
         </div>
       </div>
